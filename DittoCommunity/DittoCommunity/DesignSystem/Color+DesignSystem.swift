4a0e11c2437c429b8a5b07dcf834cc38938ce77b
realhsb <realhsb@gmail.com>
2023-11-25 16:27:38 +0900

    update: designsystem color

--- DittoCommunity/DittoCommunity/DesignSystem/Color+DesignSystem.swift
+++ DittoCommunity/DittoCommunity/DesignSystem/Color+DesignSystem.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import SwiftUI
+
+public extension Color {
+	
+	// MARK: - Gray Scale
+	static let gray01 = UIColor(named: "gray01")
+	static let gray02 = UIColor(named: "gray02")
+	static let gray03 = UIColor(named: "gray03")
+	static let gray04 = UIColor(named: "gray04")
+	static let gray05 = UIColor(named: "gray05")
+	static let gray06 = UIColor(named: "gray06")
+	static let gray07 = UIColor(named: "gray07")
+}
