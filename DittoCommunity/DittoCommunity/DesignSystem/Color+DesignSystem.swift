0591566b5228cdf7fd21e7a2fd2fddcd4128a18a
insub <vb15@kakao.com>
2023-11-25 16:00:30 +0900

    files: designsystem

---
+++ DittoCommunity/DittoCommunity/DesignSystem/Color+DesignSystem.swift
@@ -0,0 +1,8 @@
+//
+//  Color+DesignSystem.swift
+//  DittoCommunity
+//
+//  Created by 김인섭 on 11/25/23.
+//
+
+import Foundation
---
+++ DittoCommunity/DittoCommunity/DesignSystem/Font+DesignSystem.swift
@@ -0,0 +1,8 @@
+//
+//  Font+DesignSystem.swift
+//  DittoCommunity
+//
+//  Created by 김인섭 on 11/25/23.
+//
+
+import Foundation
