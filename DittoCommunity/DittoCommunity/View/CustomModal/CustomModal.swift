e30680107cfcfae7884cea46c0061be68ccb0fa8
insub <vb15@kakao.com>
2023-11-25 16:32:56 +0900

    add: Files

---
+++ DittoCommunity/DittoCommunity/View/CustomModal/CustomModal.swift
@@ -0,0 +1,8 @@
+//
+//  CustomModal.swift
+//  DittoCommunity
+//
+//  Created by 김인섭 on 11/25/23.
+//
+
+import Foundation
---
+++ DittoCommunity/DittoCommunity/View/Profile/Profile.swift
@@ -0,0 +1,8 @@
+//
+//  Profile.swift
+//  DittoCommunity
+//
+//  Created by 김인섭 on 11/25/23.
+//
+
+import Foundation
