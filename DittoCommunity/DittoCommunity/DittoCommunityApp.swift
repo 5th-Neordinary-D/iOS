2625f22ed446642b11deed2b8dcddc37e9f9b20b
insub <vb15@kakao.com>
2023-11-25 15:08:31 +0900

    init

---
+++ DittoCommunity/DittoCommunity/DittoCommunityApp.swift
@@ -0,0 +1,17 @@
+//
+//  DittoCommunityApp.swift
+//  DittoCommunity
+//
+//  Created by 김인섭 on 11/25/23.
+//
+
+import SwiftUI
+
+@main
+struct DittoCommunityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
