ad02b30553c243bca8075a2bc57086ead39807d5
insung0927 <78968690+Fiddich-Dev@users.noreply.github.com>
2023-11-25 16:40:41 +0900

    폰트이름 변경

--- DittoCommunity/DittoCommunity/DesignSystem/Font+DesignSystem.swift
+++ DittoCommunity/DittoCommunity/DesignSystem/Font+DesignSystem.swift
@@ -11,43 +11,43 @@ import SwiftUI
 
 extension Font {
     
-    public static var Heading1: Font {
-        return Font.custom("Pretendard-Bold", size: 24)
+    public static var h1: Font {
+        return .custom("Pretendard-Bold", size: 24)
     }
     
-    public static var Heading2: Font {
-        return Font.custom("Pretendard-Bold", size: 20)
+    public static var h2: Font {
+        return .custom("Pretendard-Bold", size: 20)
     }
     
-    public static var Heading3: Font {
-        return Font.custom("Pretendard-Bold", size: 16)
+    public static var h3: Font {
+        return .custom("Pretendard-Bold", size: 16)
     }
     
-    public static var Heading4: Font {
-        return Font.custom("Pretendard-Bold", size: 14)
+    public static var h4: Font {
+        return .custom("Pretendard-Bold", size: 14)
     }
     
-    public static var Title1: Font {
-        return Font.custom("Pretendard-SemiBold", size: 16)
+    public static var t1: Font {
+        return .custom("Pretendard-SemiBold", size: 16)
     }
     
-    public static var Title2: Font {
-        return Font.custom("Pretendard-SemiBold", size: 14)
+    public static var t2: Font {
+        return .custom("Pretendard-SemiBold", size: 14)
     }
     
-    public static var Body1: Font {
-        return Font.custom("Pretendard-Regular", size: 16)
+    public static var b1: Font {
+        return .custom("Pretendard-Regular", size: 16)
     }
     
-    public static var Body2: Font {
-        return Font.custom("Pretendard-Regular", size: 14)
+    public static var b2: Font {
+        return .custom("Pretendard-Regular", size: 14)
     }
     
-    public static var Body3: Font {
-        return Font.custom("Pretendard-Regular", size: 12)
+    public static var b3: Font {
+        return .custom("Pretendard-Regular", size: 12)
     }
     
-    public static var Body4: Font {
-        return Font.custom("Pretendard-Regular", size: 10)
+    public static var b4: Font {
+        return .custom("Pretendard-Regular", size: 10)
     }
 }
